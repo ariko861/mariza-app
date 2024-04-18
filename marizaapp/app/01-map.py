f6afdec3bba15b6e5fda4c52e9703aec1c4c712a
Arthur Briquet <mdpbriar@henallux.be>
2024-04-18 11:11:27 +0200

    fix: display map

--- marizaapp/app/01-map.py
+++ marizaapp/app/01-map.py
@@ -13,10 +13,10 @@ name = public_baths['properties.name']
 
 @solara.component
 def Page():
-    with solara.Sidebar():
-        solara.HTML("blabla")
+    # with solara.Sidebar():
+    #     solara.HTML("blabla")
 
-    with solara.Card():
-        px.set_mapbox_access_token(os.getenv('MAPBOX_ACCESS_TOKEN'))
-        fig = px.scatter_mapbox(public_baths, lat='lat', lon='lon', hover_name='properties.name', )
-        solara.FigurePlotly(fig)
+    # with solara.Card():
+    px.set_mapbox_access_token(os.getenv('MAPBOX_ACCESS_TOKEN'))
+    fig = px.scatter_mapbox(public_baths, lat='lat', lon='lon', hover_name='properties.name', )
+    solara.FigurePlotly(fig)
